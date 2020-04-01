62f5abb294f8e56bbb8a4f5d4269e6ba5da35f59
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-01 07:45:21 -0700

    completed reverse integer algo

---
+++ reverse_integer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace reverse_integer
+{
+  class Program
+  {
+    public static int Reverse(int x)
+    {
+      string xString = x.ToString();
+      string result = "";
+      if (x < 0)
+      {
+        result += "-";
+        for (int i = xString.Length - 1; i > 0; i--) result += xString[i];
+      }
+      else
+      {
+        for (int i = xString.Length - 1; i >= 0; i--) result += xString[i];
+      }
+      long output = Convert.ToInt64(result);
+      if (output > int.MaxValue || output < int.MinValue) return 0;
+      return (int)output;
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Reverse(123));
+      Console.WriteLine(Reverse(-123));
+      Console.WriteLine(Reverse(120));
+    }
+  }
+}
