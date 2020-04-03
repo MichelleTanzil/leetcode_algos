0eca73a060300835dae387c6a786c0c7bf1f32c1
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-03 09:27:40 -0700

    completed single number algo

---
+++ single_number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace single_number
+{
+  class Program
+  {
+    public static int SingleNumber(int[] nums)
+    {
+      return nums.GroupBy(x => x).Single(y => y.Count() == 1).Key;
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine(SingleNumber(new int[] { 2, 2, 1 }));
+      Console.WriteLine(SingleNumber(new int[] { 4, 1, 2, 1, 2 }));
+
+    }
+  }
+}
