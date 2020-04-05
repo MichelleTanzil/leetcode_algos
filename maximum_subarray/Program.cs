a2b87e3a8c199faa4d412d3a6c1504a5889d6211
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-05 07:31:52 -0700

    completed maximum subarray algo

---
+++ maximum_subarray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace maximum_subarray
+{
+  class Program
+  {
+
+    public static int MaxSubArray(int[] nums)
+    {
+      int finalSum = int.MinValue, currentSum = 0;
+      Console.WriteLine("currentSum before: " + currentSum);
+      Console.WriteLine("finalSum before: " + finalSum);
+      for (int i = 0; i < nums.Length; i++)
+      {
+        currentSum = currentSum + nums[i];
+        Console.WriteLine("currentSum: " + currentSum);
+
+        if (finalSum < currentSum)
+          finalSum = currentSum;
+
+        if (currentSum < 0)
+          currentSum = 0;
+        Console.WriteLine("currentSum: " + currentSum);
+        Console.WriteLine("finalSum: " + finalSum);
+      }
+      return finalSum;
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine(MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+      Console.WriteLine(MaxSubArray(new int[] { -1 }));
+    }
+  }
+}
