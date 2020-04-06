9a01f9a34f24725f695592d6df37afff69f25ee3
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-06 07:29:05 -0700

    completed move zeros algo

---
+++ move_zeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace move_zeros
+{
+  class Program
+  {
+    public static void MoveZeroes(int[] nums)
+    {
+      int count = 0;
+      for (int i = 0; i < nums.Length; i++)
+      {
+        // Console.WriteLine("i: " + i);
+        // Console.WriteLine("Count before: " + count);
+        if (nums[i] != 0) nums[count++] = nums[i];
+        // Console.WriteLine("Count after: " + count);
+        // for (int k = 0; k < nums.Length; k++)
+        // {
+        //   Console.WriteLine("arr: " + nums[k]);
+        // }
+      }
+      for (int j = count; j < nums.Length; j++) nums[j] = 0;
+    }
+    static void Main(string[] args)
+    {
+      MoveZeroes(new int[] { 0, 1, 0, 3, 12 });
+    }
+  }
+}
