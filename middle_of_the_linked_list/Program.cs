8081ac29afc60300abb118bd2a70685f936e3239
Michelle Tanzil <mtanzil01@gmail.com>
2020-04-10 08:29:27 -0700

    completed middle of the linked list algo

---
+++ middle_of_the_linked_list/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace middle_of_the_linked_list
+{
+  class Program
+  {
+    /**
+* Definition for singly-linked list.
+* public class ListNode {
+*     public int val;
+*     public ListNode next;
+*     public ListNode(int x) { val = x; }
+* }
+*/
+    public ListNode MiddleNode(ListNode head)
+    {
+      if (head == null) return null;
+      ListNode slow = head;
+      ListNode fast = head.next;
+
+      while (fast != null && fast.next != null)
+      {
+        fast = fast.next.next;
+        slow = slow.next;
+      }
+      return slow;
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
